633910ea9bd3911d38f9f3945df8c3a1659de909
Ayush Rao <ayushvijayrao@gmail.com>
2022-01-11 20:13:30 +0300

    Added ability to search by pincode

--- helpers/getdata.go
+++ helpers/getdata.go
@@ -41,3 +41,37 @@ func GetWeatherByCity(key, cityname string) {
 	fmt.Printf("Feel's Like: %.2fC\n", apiresponse.Main.FeelsLike)
 
 }
+
+func GetWeatherByPincode(key, pincode string) {
+	apiurl := "http://api.openweathermap.org/data/2.5/weather?units=metric&"
+
+	urltocall := fmt.Sprintf("%szip=%s,in&appid=%s", apiurl, pincode, key)
+	fmt.Println("Sending API call to:", urltocall)
+
+	response, error := http.Get(urltocall) // Sending GET request
+	if error != nil {
+		fmt.Println("An error has occurred during the API call:", error)
+		os.Exit(1)
+	}
+
+	data, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println("An error has occurred when reading the response call:", error)
+		os.Exit(1)
+	}
+
+	var apiresponse ByCityResponse
+
+	error = json.Unmarshal(data, &apiresponse)
+	if error != nil {
+		fmt.Println("An error has occurred when processing the response json:", error)
+		os.Exit(1)
+	}
+
+	fmt.Println("\nWeather data for:", apiresponse.Name)
+	fmt.Printf("Current Temperature: %.2fC\n", apiresponse.Main.Temp)
+	fmt.Printf("Minimum Temperature: %.2fC\n", apiresponse.Main.TempMin)
+	fmt.Printf("Maximum Temperature: %.2fC\n", apiresponse.Main.TempMax)
+	fmt.Printf("Feel's Like: %.2fC\n", apiresponse.Main.FeelsLike)
+
+}
