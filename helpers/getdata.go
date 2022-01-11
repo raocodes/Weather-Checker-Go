03248ebe831ff68bdfcea23894b537e0d6bd3f2f
Ayush Rao <ayushvijayrao@gmail.com>
2022-01-11 19:27:23 +0300

    Added simple CLI and api call

--- helpers/getdata.go
+++ helpers/getdata.go
@@ -2,8 +2,28 @@ package helpers
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
-func Test(){
-	fmt.Println("test")
+func GetWeatherByCity(key, cityname string) {
+	apiurl := "http://api.openweathermap.org/data/2.5/weather?"
+
+	urltocall := fmt.Sprintf("%sq=%s&appid=%s", apiurl, cityname, key)
+	fmt.Println(urltocall)
+
+	response ,error := http.Get(urltocall) // Sending GET request
+	if error != nil {
+		fmt.Println("An error has occurred during the API call:", error)
+		os.Exit(1)
+	}
+
+	data, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println("An error has occurred when reading the response call:", error)
+		os.Exit(1)
+	}
+
+	fmt.Println(data)
 }
--- weather.go
+++ weather.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/raocodes/WeatherCheckerGo/helpers"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	helpers.Test()
+	var apikey string
+	var option byte
+
+	fmt.Print("Enter your API key: ")
+	fmt.Scan(&apikey)
+	fmt.Println("The API Key being used is:", apikey)
+
+	fmt.Print("Continue (Y/N): ")
+	fmt.Scanf("%c\n", &option)
+
+	if option == 'N' || option == 'n' {
+		os.Exit(0)
+	} else if option != 'Y' && option != 'y' {
+		fmt.Println("Enter a valid option!")
+		os.Exit(0)
+	}
+
+	helpers.GetWeatherByCity(apikey, "Kuwait City")
 }
